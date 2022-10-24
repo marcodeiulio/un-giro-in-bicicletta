f1746f7aaf328aaf383200e97f2ac7afd2edc93e
marcodeiulio <mdeiulio@icloud.com>
2022-10-24 18:40:18 +0200

    init

---
+++ src/components/Carousel.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container vh-100">
+    <div class="row">
+      <div class="col-6">
+        <img
+          v-for="(image, i) in images"
+          :key="i"
+          :src="require(`@/assets/pictures/castrocaro/${image}`)"
+          :alt="image"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Carousel",
+  data() {
+    return {
+      // currentImage: 0,
+      // autoPlay: undefined,
+      images: ["castrocaro1.jpg", "castrocaro2.jpg", "castrocaro3.jpg"],
+    };
+  },
+  // methods: {
+  //   nextImage() {
+  //     this.currentImage === this.images.length - 1
+  //       ? (this.currentImage = 0)
+  //       : this.currentImage++;
+  //   },
+  //   prevImage() {
+  //     this.currentImage === 0
+  //       ? (this.currentImage = this.images.length - 1)
+  //       : this.currentImage--;
+  //   },
+  //   isActive(index) {
+  //     return index === this.currentImage;
+  //   },
+  //   setImage(index) {
+  //     this.currentImage = index;
+  //   },
+  //   startAutoPlay() {
+  //     this.autoPlay = setInterval(this.nextImage, 3000);
+  //   },
+  //   stopAutoPlay() {
+  //     clearInterval(this.autoPlay);
+  //   },
+  // },
+  // created() {
+  //   this.startAutoPlay();
+  // },
+};
+</script>
+
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -8,12 +8,12 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/route',
+    name: 'Route',
     // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
+    // this generates a separate chunk (route.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "route" */ '../views/Route.vue')
   }
 ]
 
